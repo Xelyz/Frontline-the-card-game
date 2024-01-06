d34d6d01303a53e7fda80d2a13ed8f91e6b9a6cd
Xelyz <wildon.wu.you@icloud.com>
2024-01-06 10:44:26 +0800

    Update origin

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { Server, Origins } from 'boardgame.io/server';
 import path from 'path';
 import serve from 'koa-static';
 import { Cardgame } from './src/Game.js';
-const server = Server({ games: [Cardgame], origins:['http://localhost:3000', 'http://localhost:8000', Origins.LOCALHOST]});
+const server = Server({ games: [Cardgame], origins:['http://localhost:3000', '*', Origins.LOCALHOST]});
 const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
