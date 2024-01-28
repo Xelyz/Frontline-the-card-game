93a1d163e9d8098a67b06d21fec718c6faf21388
Xelyz <wildon.wu.you@icloud.com>
2024-01-28 09:25:01 +0800

    Add resources

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 import { Server, Origins } from 'boardgame.io/server';
-import path from 'path';
-import serve from 'koa-static';
 import { Cardgame } from './src/Game.js';
-const server = Server({ games: [Cardgame], origins:['http://localhost:3000', '*', Origins.LOCALHOST]});
+
+const server = Server({ games: [Cardgame], origins:['https://frontline-the-card-game.pages.dev', '*', Origins.LOCALHOST]});
 const PORT = process.env.PORT || 8000;
 
-// Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, './build');
-server.app.use(serve(frontEndAppBuildPath));
-server.run(PORT, () => {
-  server.app.use(
-    async (ctx, next) =>
-      await serve(frontEndAppBuildPath)(
-        Object.assign(ctx, { path: 'index.html' }),
-        next
-      )
-  );
-});
+server.run(PORT);
