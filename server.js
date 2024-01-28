6d3ddafe0064f281754392f60e2d2e47b4cfa88c
Xelyz <wildon.wu.you@icloud.com>
2024-01-28 13:42:05 +0800

    revert changes

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import { Server, Origins } from 'boardgame.io/server';
+import path from 'path';
+import serve from 'koa-static';
 import { Cardgame } from './src/Game.js';
-
 const server = Server({ games: [Cardgame], origins:['https://frontline-the-card-game.pages.dev', '*', Origins.LOCALHOST]});
 const PORT = process.env.PORT || 8000;
 
-server.run(PORT);
+// Build path relative to the server.js file
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+server.app.use(serve(frontEndAppBuildPath));
+server.run(PORT, () => {
+  server.app.use(
+    async (ctx, next) =>
+      await serve(frontEndAppBuildPath)(
+        Object.assign(ctx, { path: 'index.html' }),
+        next
+      )
+  );
+});
