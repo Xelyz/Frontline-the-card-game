9d1f14e73ccb23829c6aa9b53ae64ab5fd6c8eb7
Xelyz <wildon.wu.you@icloud.com>
2024-02-09 09:08:38 +0800

    slight modification

--- src/BackgroundWrapper.js
+++ src/BackgroundWrapper.js
@@ -7,7 +7,6 @@ function Background(Component){
                 <div className='fixed container h-screen left-1/2 -translate-x-1/2 px-4 bg-center brightness-[0.9] contrast-125' style={{backgroundImage: 'url(/imgPack/bg.png)'}}/>
                 <div className='container h-screen mx-auto px-4 relative'>
                     {/* general background/heading design */}
-                    <p className='text-center font-mono text-2xl text-white absolute top-1 w-full left-0'>Frontline</p>
                     <Component {...props}/>
                 </div>
             </>
