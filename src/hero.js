2aac46feda27e151dbdb1b4a729509aae336faae
Xelyz <wildon.wu.you@icloud.com>
2024-02-12 17:16:35 +0800

    Change health of heros

--- src/hero.js
+++ src/hero.js
@@ -3,7 +3,7 @@ export const HEROS=[
     {
         name: "Seraphina",
         atk: 0,
-        hp: 15,
+        hp: 20,
         kind: 'hero',
         color: 'green',
         desc: 'Once every turn of yours, you can right click an empty tile to set a 0/1 trap that deals 1 damage to whoever steps on it, only visible by you',
@@ -15,7 +15,7 @@ export const HEROS=[
     {
         name: "Riven",
         atk: 2,
-        hp: 15,
+        hp: 20,
         kind: 'hero',
         color: 'blue',
         desc: '**Permanently has 2 atk**',
