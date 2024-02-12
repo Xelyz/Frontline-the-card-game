573842e4f6a4e4953c876ba896fc8e76bbb3a9f3
Xelyz <wildon.wu.you@icloud.com>
2024-02-12 17:13:11 +0800

    bug fix

--- src/hero.js
+++ src/hero.js
@@ -1,20 +1,14 @@
-function heroProperty(hero){
-    return {
-        onOut: 'lose',
-        kind: 'hero',
-        ...hero
-    }
-}
-
 /**the list of all heros */
 export const HEROS=[
     {
         name: "Seraphina",
         atk: 0,
         hp: 15,
+        kind: 'hero',
         color: 'green',
         desc: 'Once every turn of yours, you can right click an empty tile to set a 0/1 trap that deals 1 damage to whoever steps on it, only visible by you',
         effect: {
+            onOut: 'lose'
         },
         img: 'url(/imgPack/Seraphina.png)',
     },
@@ -22,10 +16,12 @@ export const HEROS=[
         name: "Riven",
         atk: 2,
         hp: 15,
+        kind: 'hero',
         color: 'blue',
         desc: '**Permanently has 2 atk**',
         effect: {
+            onOut: 'lose'
         },
         img: 'url(/imgPack/Riven.png)',
     },
-].map(heroProperty)
+]
