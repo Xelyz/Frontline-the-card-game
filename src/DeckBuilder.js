6aba13b92529c0e1d7e452e29a346b6e39144b89
Xelyz <wildon.wu.you@icloud.com>
2024-02-13 21:21:47 +0800

    Add display of current deck, enable adding to and removing cards from the deck

--- src/DeckBuilder.js
+++ src/DeckBuilder.js
@@ -3,18 +3,6 @@ import { CARDS } from "./Cards";
 import { withRouter } from "./RouterWrapper";
 import Background from "./BackgroundWrapper";
 
-/**
- * String interpreter
- * Sample string:
- * "
- * hero green
- * 2x Mouse
- * 1x Boxer
- * ...
- * "
- * total card: 25
- */
-
 class DeckBuilder extends Component{
     state = {
         deck: {},
@@ -26,16 +14,20 @@ class DeckBuilder extends Component{
         super(props)
         this.allowDrag = this.allowDrag.bind(this)
         this.handleDrag = this.handleDrag.bind(this)
-        this.handleDragOver = this.handleDragOver.bind(this)
+        this.handleDragEnter = this.handleDragEnter.bind(this)
+        this.handleDragOut = this.handleDragOut.bind(this)
         this.handleDrop = this.handleDrop.bind(this)
         this.release = this.release.bind(this)
+        this.clearDeck = this.clearDeck.bind(this)
+        this.removeCardFromDeck = this.removeCardFromDeck.bind(this)
     }
 
     allowDrag(e){
         this.setState({dragging: e.target.id})
         this.setState({mousePosition: [e.clientX, e.clientY]})
-        document.getElementById('deckbar').addEventListener('mouseover', this.handleDragOver)
+        document.getElementById('deckbar').addEventListener('mouseenter', this.handleDragEnter)
         document.getElementById('deckbar').addEventListener('mouseup', this.handleDrop)
+        document.addEventListener('mousemove', this.handleDrag);
         window.addEventListener('mouseup', this.release)
     }
 
@@ -53,13 +45,28 @@ class DeckBuilder extends Component{
         }
     }
 
-    handleDragOver(e){
-        console.log('over')
+    handleDragEnter(e){
+        e.currentTarget.style.backgroundColor = 'rgba(180, 83, 9, 0.5)'
+    }
+
+    handleDragOut(e){
+        e.currentTarget.style.backgroundColor = 'rgba(245, 158, 11, 0.5)'
     }
 
     handleDrop(e){
         const id = this.state.dragging
-        console.log(id)
+        let num = this.state.deck[id] || 0
+        if(Object.values(this.state.deck).reduce((accumulator, currentValue) => accumulator + currentValue, 0) >= 30){
+            
+        }
+        else if(num<2){
+            this.setState({
+                deck: {
+                    ...this.state.deck,
+                    [id]: num+1
+                }
+            })
+        }
     }
 
     release(e){
@@ -68,41 +75,75 @@ class DeckBuilder extends Component{
         draggedElement.style.zIndex = 0
         draggedElement.style.cursor = 'grab'
         this.setState({dragging: false})
-        document.getElementById('deckbar').removeEventListener('mouseover', this.handleDragOver)
+        document.getElementById('deckbar').removeEventListener('mouseenter', this.handleDragEnter)
         document.getElementById('deckbar').removeEventListener('mouseup', this.handleDrop)
+        document.removeEventListener('mousemove', this.handleDrag);
         window.removeEventListener('mouseup', this.release)
     }
 
     cardList(){
         const card = CARDS.map((card)=>
-        <div id={card.id}
-        className='relative rounded-md bg-white/40 shadow-lg h-60 w-60 box-content text-center select-none cursor-grab'
-        style={{backgroundImage: card.img, backgroundSize: 'cover'}}
-        onMouseDown={this.allowDrag}
-        onMouseMove={this.handleDrag}>
-            <p className='atk text-2xl' style={{color: 'cyan'}}>{card.atk}</p>
-            <p className='hp text-2xl' style={{color: 'cyan'}}>{card.hp}</p>
-            <p className='cost text-2xl' style={{color: 'violet'}}>{card.cost}</p>
-            <p className='absolute -bottom-7 text-xl left-1/2 px-1 -translate-x-1/2 w-fit text-white bg-black/25'>{card.name}</p>
+        <div>
+            <div id={card.id}
+            className='relative rounded-md bg-white/40 shadow-lg h-60 w-60 box-content text-center select-none cursor-grab'
+            style={{backgroundImage: card.img, backgroundSize: 'cover'}}
+            onMouseDown={this.allowDrag}
+            >
+                <p className='atk text-2xl' style={{color: 'cyan'}}>{card.atk}</p>
+                <p className='hp text-2xl' style={{color: 'cyan'}}>{card.hp}</p>
+                <p className='cost text-2xl' style={{color: 'violet'}}>{card.cost}</p>
+            </div>
+            <p className='h-7 text-xl w-60 text-center px-1 text-white bg-black/25'>{card.name}</p>
         </div>)
         return (
-            <div className='grid grid-cols-4 h-screen w-4/5 gap-y-8 overflow-y-scroll overflow-x-visible'>
+            <div className='grid pt-20 grid-cols-4 h-screen w-full gap-y-2 overflow-y-scroll overflow-x-visible'>
                 {card}
             </div>
         )
     }
 
+    clearDeck(e){
+        this.setState({deck: {}})
+    }
+
+    removeCardFromDeck(e){
+        const id = e.currentTarget.id.slice(1)
+        console.log(id)
+        this.setState({
+            deck: {
+                ...this.state.deck,
+                [id]: this.state.deck[id]-1
+            }
+        })
+    }
+
     deckBar(){
+        let cardInDeck = []
+        for(let id in this.state.deck){
+            if(this.state.deck[id]>0){
+                const card = CARDS.find(card=>card.id == id)
+                const bg = {'minion': 'bg-blue-300', 'spell': 'bg-blue-500', 'trap': 'bg-sky-600'}[card.kind]
+                cardInDeck.push(<div id={`d${id}`} className='h-10 m-1 border border-black flex flex-row rounded' onClick={this.removeCardFromDeck}>
+                    <div className='h-full aspect-square bg-black text-white text-center text-xl'>{this.state.deck[id]}</div>
+                    <div className={`text-2xl ${bg} text-center flex-grow`}>{card.name}</div>
+                </div>)
+            }
+        }
+
         return (
-            <div id='deckbar' className='w-1/6 bg-amber-500/50 backdrop-blur-sm border-8 border-black select-none cursor-default'>
+            <div id='deckbar' className='w-1/5 bg-amber-500/50 backdrop-blur-sm border-8 border-black select-none cursor-default relative'
+            onMouseOut={this.handleDragOut}>
                 <div className='w-full bg-white text-center text-2xl rounded'>Your deck</div>
+                <button className='h-10 px-2 absolute bottom-1 left-1 text-2xl rounded bg-white' onClick={this.clearDeck}>Clear</button>
+                <p className='absolute bottom-2 right-2 text-2xl text-white'>{`${Object.values(this.state.deck).reduce((accumulator, currentValue) => accumulator + currentValue, 0)}/30`}</p>
+                {cardInDeck}
             </div>
         )
     }
 
     render(){
         return (
-            <div className='flex flex-row justify-between'>
+            <div className='flex flex-row'>
                 {this.cardList()}
                 {this.deckBar()}
             </div>
