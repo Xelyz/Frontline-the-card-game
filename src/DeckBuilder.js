44b743281e5f8625b49cb7ceae27d3a9a6d0c644
Xelyz <wildon.wu.you@icloud.com>
2024-02-18 17:55:37 +0800

    Complete funtionality of the deckbuilder

--- src/DeckBuilder.js
+++ src/DeckBuilder.js
@@ -8,6 +8,8 @@ class DeckBuilder extends Component{
         deck: {},
         dragging: false,
         mousePosition: [0,0],
+        preview: false,
+        copied: false,
     }
 
     constructor(props){
@@ -20,15 +22,32 @@ class DeckBuilder extends Component{
         this.release = this.release.bind(this)
         this.clearDeck = this.clearDeck.bind(this)
         this.removeCardFromDeck = this.removeCardFromDeck.bind(this)
+        this.copyDeck = this.copyDeck.bind(this)
+    }
+
+    preventDefault(e){
+        e.preventDefault()
+    }
+
+    componentDidMount(){
+        window.addEventListener('contextmenu', this.preventDefault)
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('contextmenu', this.preventDefault)
     }
 
     allowDrag(e){
-        this.setState({dragging: e.target.id})
-        this.setState({mousePosition: [e.clientX, e.clientY]})
-        document.getElementById('deckbar').addEventListener('mouseenter', this.handleDragEnter)
-        document.getElementById('deckbar').addEventListener('mouseup', this.handleDrop)
-        document.addEventListener('mousemove', this.handleDrag);
-        window.addEventListener('mouseup', this.release)
+        if(e.ctrlKey){
+            this.setState({preview: e.target.id})
+        }else{
+            this.setState({dragging: e.target.id})
+            this.setState({mousePosition: [e.clientX, e.clientY]})
+            document.getElementById('deckbar').addEventListener('mouseenter', this.handleDragEnter)
+            document.getElementById('deckbar').addEventListener('mouseup', this.handleDrop)
+            document.addEventListener('mousemove', this.handleDrag);
+            window.addEventListener('mouseup', this.release)
+        }
     }
 
     handleDrag(e) {
@@ -93,10 +112,10 @@ class DeckBuilder extends Component{
                 <p className='hp text-2xl' style={{color: 'cyan'}}>{card.hp}</p>
                 <p className='cost text-2xl' style={{color: 'violet'}}>{card.cost}</p>
             </div>
-            <p className='h-7 text-xl w-60 text-center px-1 text-white bg-black/25'>{card.name}</p>
+            <p className='h-7 text-xl w-60 text-center px-1 text-white bg-black/25 select-none cursor-default'>{card.name}</p>
         </div>)
         return (
-            <div className='grid pt-20 grid-cols-4 h-screen w-full gap-y-2 overflow-y-scroll overflow-x-visible'>
+            <div className='grid mt-5 grid-cols-4 w-full gap-y-2 overflow-y-scroll overflow-x-visible'>
                 {card}
             </div>
         )
@@ -106,24 +125,68 @@ class DeckBuilder extends Component{
         this.setState({deck: {}})
     }
 
-    removeCardFromDeck(e){
-        const id = e.currentTarget.id.slice(1)
-        console.log(id)
-        this.setState({
-            deck: {
-                ...this.state.deck,
-                [id]: this.state.deck[id]-1
+    copyDeck(e){
+        if(Object.values(this.state.deck).reduce((accumulator, currentValue) => accumulator + currentValue, 0) === 30){
+            let deckInfo = ''
+            for(let id in this.state.deck){
+                let str = parseInt(id).toString(16)
+                console.log(str)
+                if(str.length<2){
+                    str = '0'+str
+                }
+                deckInfo += str.repeat(this.state.deck[id])
             }
-        })
+            if(!navigator.clipboard){
+                document.execCommand('copy', false, deckInfo);
+            }
+            else{
+                navigator.clipboard.writeText(deckInfo).then(
+                function(){
+                    
+                })
+                .catch(
+                function() {
+        
+                });
+            }
+            
+            this.setState({ copied: true });
+            setTimeout(
+                function () {
+                this.setState({ copied: false });
+                }.bind(this),
+                1000
+            );
+        }else{
+            document.getElementById('hint').innerHTML = 'Export deck when it contains exactly 30 cards'
+            setTimeout(() => {
+                document.getElementById('hint').innerHTML = 'Ctrl+Click on card to show card information'
+            }, 1500);
+        }
+    }
+
+    removeCardFromDeck(e){
+        if(e.ctrlKey){
+            this.setState({preview: e.currentTarget.id.slice(1)})
+        }else{
+            const id = e.currentTarget.id.slice(1)
+            console.log(id)
+            this.setState({
+                deck: {
+                    ...this.state.deck,
+                    [id]: this.state.deck[id]-1
+                }
+            })
+        }
     }
 
     deckBar(){
         let cardInDeck = []
         for(let id in this.state.deck){
             if(this.state.deck[id]>0){
-                const card = CARDS.find(card=>card.id == id)
+                const card = CARDS.find(card=>card.id.toString() === id)
                 const bg = {'minion': 'bg-blue-300', 'spell': 'bg-blue-500', 'trap': 'bg-sky-600'}[card.kind]
-                cardInDeck.push(<div id={`d${id}`} className='h-10 m-1 border border-black flex flex-row rounded' onClick={this.removeCardFromDeck}>
+                cardInDeck.push(<div id={`d${id}`} className='h-10 m-1 border border-black flex flex-row rounded' onClick={this.removeCardFromDeck} onContextMenu={e=>e.preventDefault()}>
                     <div className='h-full aspect-square bg-black text-white text-center text-xl'>{this.state.deck[id]}</div>
                     <div className={`text-2xl ${bg} text-center flex-grow`}>{card.name}</div>
                 </div>)
@@ -131,22 +194,60 @@ class DeckBuilder extends Component{
         }
 
         return (
-            <div id='deckbar' className='w-1/5 bg-amber-500/50 backdrop-blur-sm border-8 border-black select-none cursor-default relative'
-            onMouseOut={this.handleDragOut}>
+            <div id='deckbar' className='bg-amber-500/50 basis-1/5 h-full backdrop-blur-sm border-8 border-black select-none cursor-default relative flex flex-col' onMouseOut={this.handleDragOut}>
                 <div className='w-full bg-white text-center text-2xl rounded'>Your deck</div>
-                <button className='h-10 px-2 absolute bottom-1 left-1 text-2xl rounded bg-white' onClick={this.clearDeck}>Clear</button>
-                <p className='absolute bottom-2 right-2 text-2xl text-white'>{`${Object.values(this.state.deck).reduce((accumulator, currentValue) => accumulator + currentValue, 0)}/30`}</p>
-                {cardInDeck}
+                <div className='grow overflow-y-scroll'>{cardInDeck}</div>
+                <div className='flex h-10 m-2 text-2xl justify-between'>
+                    <button className='h-10 px-2 rounded bg-white hover:bg-slate-300 hover:scale-105 transition-transform' onClick={this.clearDeck}>Clear</button>
+                    <button className='h-10 px-2 rounded bg-lime-400 hover:bg-lime-500 hover:scale-105 transition-transform' onClick={this.copyDeck}>{this.state.copied?'Copied!':'Export'}</button>
+                    <p className='text-white'>{`${Object.values(this.state.deck).reduce((accumulator, currentValue) => accumulator + currentValue, 0)}/30`}</p>
+                </div>
             </div>
         )
     }
 
+    preview(){
+        function exit(e){
+            this.setState({preview: false})
+        }
+        const cardSelected = CARDS.find(card => card.id.toString() === this.state.preview)
+        return <div id='cardPreview' className='absolute top-0 left-0 w-full h-full z-50 bg-black/40' onClick={exit.bind(this)}>
+            <div className='absolute top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2'>
+                <div className='relative rounded-md bg-white shadow-lg h-[500px] w-80 my-7 box-content text-center'>
+                    <div className='w-80 h-80 rounded-md bg-slate-200' style={{backgroundImage: cardSelected.img, backgroundSize: 'cover'}}/>
+                    <p className='atk text-3xl' style={{color: 'cyan'}}>{cardSelected.atk}</p>
+                    <p className='hp text-3xl' style={{color: 'cyan'}}>{cardSelected.hp}</p>
+                    <p className='cost text-3xl' style={{color: 'violet'}}>{cardSelected.cost}</p>
+                    <p className='absolute bottom-0 text-2xl font-mono w-full'>{cardSelected.kind}</p>
+                    <div>
+                        <p className='w-full text-3xl'>{cardSelected.name}</p>
+                        <span>{cardSelected.desc}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
+
     render(){
         return (
-            <div className='flex flex-row'>
-                {this.cardList()}
-                {this.deckBar()}
-            </div>
+            <>
+                <div className='flex flex-row h-full'>
+                    <div className='flex flex-col basis-4/5'>
+                        <div className='flex text-3xl h-14 bg-white/40 backdrop-blur-sm'>
+                            <button
+                            className="h-14 px-4 bg-white rounded hover:bg-slate-300 hover:scale-105 transition-transform"
+                            onClick={() => this.props.navigate('/')}
+                            >
+                                Return
+                            </button>
+                            <p id='hint' className='px-4'>Ctrl+Click on card to show card information</p>
+                        </div>
+                        {this.cardList()}
+                    </div>
+                    {this.deckBar()}
+                </div>
+                {this.state.preview?this.preview():''}
+            </>
         )
     }
 }
